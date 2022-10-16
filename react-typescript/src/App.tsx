ecae254caa5657b1e60ba390cb3c73c0aacf5c7a
bulidiriba <bulidiriba@gmail.com>
2022-10-16 14:31:43 +0300

    add some mui components

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Box, CssBaseline, Paper, Typography } from "@mui/material"; 
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <CssBaseline />
+      <Box
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <Paper
+          elevation={3}
+          sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
+        >
+          <Typography color="primary.dark" variant="h1">
+            Starter App
+          </Typography>
+        </Paper>
+      </Box>
     </div>
   );
 }
